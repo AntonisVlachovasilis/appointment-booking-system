1f2b541ff153c90d0a7c42a62dc0fcd0834b3217
AvJrdev <144242281+AvJrdev@users.noreply.github.com>
2023-12-30 22:15:55 +0200

    route middleware

--- appointment-system/routes/web.php
+++ appointment-system/routes/web.php
@@ -16,21 +16,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/appointment', [AppointmentController::class, 'index'])->name('appointment.index');
-Route::get('/appointment/create', [AppointmentController::class, 'create'])->name('appointment.create');
-Route::post('/appointment', [AppointmentController::class, 'store'])->name('appointment.store');
-Route::get('/appointment/{appointment}/edit', [AppointmentController::class, 'edit'])->name('appointment.edit');
-Route::put('/appointment/{appointment}/update', [AppointmentController::class, 'update'])->name('appointment.update');
-Route::delete('/appointment/{appointment}/destroy', [AppointmentController::class, 'destroy'])->name('appointment.destroy');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+
+    Route::get('/appointment', [AppointmentController::class, 'index'])->name('appointment.index');
+    Route::get('/appointment/create', [AppointmentController::class, 'create'])->name('appointment.create');
+    Route::post('/appointment', [AppointmentController::class, 'store'])->name('appointment.store');
+    Route::get('/appointment/{appointment}/edit', [AppointmentController::class, 'edit'])->name('appointment.edit');
+    Route::put('/appointment/{appointment}/update', [AppointmentController::class, 'update'])->name('appointment.update');
+    Route::delete('/appointment/{appointment}/destroy', [AppointmentController::class, 'destroy'])->name('appointment.destroy');
+
+
+
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
